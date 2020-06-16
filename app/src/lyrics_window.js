947f68d1260bc631851bf960e5346c9d3c654100
Mageer <mageer.github@gmail.com>
2020-06-17 01:05:32 +0300

    Removed unnecessary internal state

--- app/src/lyrics_window.js
+++ app/src/lyrics_window.js
@@ -4,26 +4,28 @@ import SearchButton from './search_button'
 export class LyricsWindow extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            search: 'hey ya',
+        this.state = {
             name: null,
             lyrics: null,
         };
     }
 
+
     get_lyrics = (search) => {
         this.setState({name: "Please wait for lyrics...", lyrics: null});
+
         fetch("/lyrics?search=" + encodeURI(search))
-        .then((res) => res.json())
-        .then(({name, lyrics}) => this.setState({name, lyrics}))
-        .catch((err) => this.setState({name: "No matching song found..."}));
+            .then((res) => res.json())
+            .then(({name, lyrics}) => this.setState({name, lyrics}))
+            .catch((err) => this.setState({name: "No matching song found..."}));
     }
 
+
     handleClick = (value) => {
-        this.setState({songName: value});
         this.get_lyrics(value);
     }
 
+    
     render() {
         return (
             <div>
