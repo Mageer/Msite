a854f119870da7174379216fc536243f67bd8482
Mageer <mageer.github@gmail.com>
2020-10-01 17:21:24 +0300

    Added username next to logout

--- app/src/components/auth/Logout.js
+++ app/src/components/auth/Logout.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import CustomButton from "../misc/CustomButton";
 import { logoutUser } from "../../actions/user";
 
-function Logout() {
-  const dispatch = useDispatch();
+const useStyles = makeStyles({
+  name: {
+    paddingRight: "5px",
+    paddingTop: "3px",
+  },
+});
 
-  const handleClick = () => {
-    console.log("log out");
-    dispatch(logoutUser());
-  };
+function Logout() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const username = useSelector((state) => state.user.username);
+  const name = username.replace("spotify_", "");
+
+  const handleClick = () => dispatch(logoutUser());
 
   return (
-    <CustomButton onClick={handleClick}>
-      <ExitToAppIcon />
-    </CustomButton>
+    <>
+      <Typography className={classes.name}>{name}</Typography>
+      <CustomButton onClick={handleClick}>
+        <ExitToAppIcon />
+      </CustomButton>
+    </>
   );
 }
 
